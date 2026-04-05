35fe65b2b7f4a48edfc5033b9b41bd8655ddcc74
Srishti <srishtijain906@gmail.com>
2026-04-05 21:46:20 +0530

    Add files

---
+++ q44.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct Node *queue[1000];
+int front = 0, rear = 0;
+
+void enqueue(struct Node *node)
+{
+    queue[rear++] = node;
+}
+
+struct Node *dequeue()
+{
+    return queue[front++];
+}
+
+struct Node *buildTree(int arr[], int n)
+{
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node *root = createNode(arr[0]);
+    enqueue(root);
+
+    int i = 1;
+
+    while (i < n)
+    {
+        struct Node *current = dequeue();
+
+        if (i < n && arr[i] != -1)
+        {
+            current->left = createNode(arr[i]);
+            enqueue(current->left);
+        }
+        i++;
+
+        if (i < n && arr[i] != -1)
+        {
+            current->right = createNode(arr[i]);
+            enqueue(current->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void inorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+void preorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node *root = buildTree(arr, n);
+
+    inorder(root);
+    printf("\n");
+
+    preorder(root);
+    printf("\n");
+
+    postorder(root);
+
+    return 0;
+}
