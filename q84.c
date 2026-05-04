fe0802197179c26553082c0691810e2291133723
Srishti <srishtijain906@gmail.com>
2026-05-04 17:24:58 +0530

    Add files

---
+++ q84.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    // Input size of array
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // Insertion Sort
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Shift larger elements one position ahead
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // Insert key at correct position
+        arr[j + 1] = key;
+    }
+
+    // Print sorted array
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
